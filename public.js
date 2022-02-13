014b2c93483b07857ba94322248abe7fcfe9cfaf
Ali Yasin <46627616+aliyasiny65@users.noreply.github.com>
2022-02-13 17:47:08 +0300

    Update public.js

--- public.js
+++ public.js
@@ -1,6 +1,6 @@
 document.getElementById("toggle").addEventListener("click", function(){
     document.getElementsByTagName('body')[0].classList.toggle("dark-theme");
-    document.cookie = "theme=dark"
+    document.cookie = "theme=dark; expires=Thu, 18 Dec 2023 12:00:00 UTC"
 });
 let theme = document.cookie;
 if(theme === "theme=dark") {
