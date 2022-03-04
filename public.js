77aeedea6e2d588709a66cb9ba46bd5feb016a07
Ali Yasin <46627616+aliyasiny65@users.noreply.github.com>
2022-03-04 22:06:03 +0300

    Update public.js

--- public.js
+++ public.js
@@ -1,6 +1,16 @@
 let theme = document.cookie;
 if(theme === "theme=dark") {
-   document.getElementsByTagName('body')[0].classList.toggle("dark-theme"); 
+   document.getElementById("toggle").addEventListener("click", function(){
+   document.getElementsByTagName('body')[0].classList.toggle("dark-theme");
+   document.cookie = "theme=light; expires=Thu, 18 Dec 2023 12:00:00 UTC"
+   document.getElementsByTagName('body')[0].classList.toggle("dark-theme");
+});
+} else if (theme === "theme=light") {
+    document.getElementById("toggle").addEventListener("click", function(){
+    document.getElementsByTagName('body')[0].classList.toggle("dark-theme");
+    document.cookie = "theme=dark; expires=Thu, 18 Dec 2023 12:00:00 UTC"
+    document.getElementsByTagName('body')[0].classList.toggle("");
+});
 } else {
     document.getElementById("toggle").addEventListener("click", function(){
     document.getElementsByTagName('body')[0].classList.toggle("dark-theme");
