19cd8e94a580d7b76caf37b8c3748a3d8f34a553
Ali Yasin <46627616+aliyasiny65@users.noreply.github.com>
2022-03-05 23:12:25 +0300

    Update public.js

--- public.js
+++ public.js
@@ -1,13 +1,24 @@
-let theme = document.cookie;
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+let theme = getCookie("theme");
 let sistemtema = window.matchMedia('(prefers-color-scheme: dark)').matches;
-if(theme === "theme=dark") {
+if(theme === "dark") {
    document.getElementsByTagName('body')[0].classList.toggle("dark-theme"); 
    document.getElementById("toggle").addEventListener("click", function(){
    document.getElementsByTagName('body')[0].classList.toggle("#");
    document.cookie = "theme=light; expires=Thu, 18 Dec 2023 12:00:00 UTC"
    document.getElementsByTagName('body')[0].classList.toggle("dark-theme");
 });
-} else if (theme === "theme=light") {
+} else if (theme === "light") {
     document.getElementById("toggle").addEventListener("click", function(){
     document.getElementsByTagName('body')[0].classList.toggle("dark-theme");
     document.cookie = "theme=dark; expires=Thu, 18 Dec 2023 12:00:00 UTC"
