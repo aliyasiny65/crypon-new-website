eabdb9b8918b6c812fc25f8ca3eb7520fd1c9ddb
Ali Yasin <46627616+aliyasiny65@users.noreply.github.com>
2022-03-05 12:23:10 +0300

    Update public.js

--- public.js
+++ public.js
@@ -1,36 +1,27 @@
 let theme = document.cookie;
-
 let sistemtema = window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-if (theme !== "theme=dark" || "theme=light") {
-if(sistemtema) {
-   document.getElementsByTagName('body')[0].classList.toggle("dark-theme");
+if(theme === "theme=dark") {
+   document.getElementsByTagName('body')[0].classList.toggle("dark-theme"); 
    document.getElementById("toggle").addEventListener("click", function(){
-   document.getElementsByTagName('body')[0].classList.toggle("dark-theme");
+   document.getElementsByTagName('body')[0].classList.toggle("#");
    document.cookie = "theme=light; expires=Thu, 18 Dec 2023 12:00:00 UTC"
-}); 
-} else {
-    document.getElementById("toggle").addEventListener("click", function(){
-    document.getElementsByTagName('body')[0].classList.toggle("dark-theme");
-    document.cookie = "theme=dark; expires=Thu, 18 Dec 2023 12:00:00 UTC"
-    document.getElementsByTagName('body')[0].classList.toggle("");
-});
-}
-} else if(theme === "theme=dark") {
    document.getElementsByTagName('body')[0].classList.toggle("dark-theme");
-   document.getElementById("toggle").addEventListener("click", function(){
-   document.getElementsByTagName('body')[0].classList.toggle("dark-theme");
-   document.cookie = "theme=light; expires=Thu, 18 Dec 2023 12:00:00 UTC"
 });
 } else if (theme === "theme=light") {
     document.getElementById("toggle").addEventListener("click", function(){
     document.getElementsByTagName('body')[0].classList.toggle("dark-theme");
     document.cookie = "theme=dark; expires=Thu, 18 Dec 2023 12:00:00 UTC"
-    document.getElementsByTagName('body')[0].classList.toggle("");
+});
+} else if (sistemtema) {
+    document.getElementsByTagName('body')[0].classList.toggle("dark-theme");
+    document.cookie = "theme=dark; expires=Thu, 18 Dec 2023 12:00:00 UTC"
+    document.getElementById("toggle").addEventListener("click", function(){
+    document.getElementsByTagName('body')[0].classList.toggle("#toggle");
+    document.cookie = "theme=light; expires=Thu, 18 Dec 2023 12:00:00 UTC"
 });
 } else {
     document.getElementById("toggle").addEventListener("click", function(){
     document.getElementsByTagName('body')[0].classList.toggle("dark-theme");
     document.cookie = "theme=dark; expires=Thu, 18 Dec 2023 12:00:00 UTC"
-});
+})
 }
